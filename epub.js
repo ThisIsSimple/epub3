7be0eac2b69606992a1c264f509392a71e17ac4c
kal501 <69739898+kal501@users.noreply.github.com>
2022-06-06 01:15:53 +0900

    Initial Commit

---
+++ epub.js
@@ -0,0 +1,45 @@
+"use strict";
+const tslib_1 = require("tslib");
+const epub_1 = (0, tslib_1.__importDefault)(require("./lib/epub"));
+module.exports = epub_1.default;
+/*
+// @ts-ignore
+declare module "epub"
+{
+
+    import { EventEmitter } from "events";
+
+    interface TocElement
+    {
+        level: number;
+        order: number;
+        title: string;
+        id: string;
+        href?: string;
+    }
+
+    class EPub extends EventEmitter
+    {
+        constructor(epubfile: string, imagewebroot?: string, chapterwebroot?: string);
+
+        metadata: Object;
+        manifest: Object;
+        spine: Object;
+        flow: Array<Object>;
+        toc: Array<TocElement>;
+
+        parse(): void;
+
+        getChapter(chapterId: string, callback: (error: Error, text: string) => void): void;
+
+        getChapterRaw(chapterId: string, callback: (error: Error, text: string) => void): void;
+
+        getImage(id: string, callback: (error: Error, data: Buffer, mimeType: string) => void): void;
+
+        getFile(id: string, callback: (error: Error, data: Buffer, mimeType: string) => void): void;
+    }
+
+    export = EPub;
+}
+*/
+//# sourceMappingURL=epub.js.map
